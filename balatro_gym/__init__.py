6a1680ab1cdcc5f27e97c22e154fc5fddfb9732e
Cassius Villareal <cassiusfive@gmail.com>
2024-03-17 04:38:35 -0700

    🎨 Tabs -> Spaces

--- balatro_gym/__init__.py
+++ balatro_gym/__init__.py
@@ -1,6 +1,6 @@
 from gymnasium.envs.registration import register
 
 register(
-	id="Balatro-v0",
-	entry_point="balatro_gym:BalatroEnv"
+    id="Balatro-v0",
+    entry_point="balatro_gym:BalatroEnv"
 )
